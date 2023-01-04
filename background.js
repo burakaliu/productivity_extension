ef23be9bf2c56083bbee8a4f2856173b4aeec574
Burak Unlu <burakaliunlu@gmail.com>
2023-01-04 13:02:02 -0500

    copy pasted some random stuff from stack overflow and hope it works

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 //dormant until an event the file is listening for fires, react with specified instructions, then unload.
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    /*
     if (tab.url && tab.url.includes("youtube.com/watch")){
         const queryParameters = tab.url.split("?")[1];
         const  urlParameters = new URLSearchParams(queryParameters);
@@ -10,7 +11,22 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
             videoId: urlParameters.get("v")
         });
     }
-    //if tab does not already exist, create a new tab object with the tab's url and a time value of 0
-
+    */
+   // by passing an object you can define default values e.g.: []
+    chrome.storage.local.get({tabs: []}, function (result) {
+        // the input argument is ALWAYS an object containing the queried keys
+        // so we select the key we need
+        var tabs = result.tabs;
+        tabs.push({keyPairId: keyPairId, HasBeenUploadedYet: false});
+        // set the new array value to the same key
+        chrome.storage.local.set({tabs: tabs}, function () {
+            // you can use strings instead of objects
+            // if you don't  want to define default values
+            chrome.storage.local.get('tabs', function (result) {
+                console.log(result.tabs)
+            });
+        });
+    })
     
+
 })
